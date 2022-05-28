b150ade29d08bf6f1ae4ed1c0e08b64856d85199
Akbar S <54757270+akbartus@users.noreply.github.com>
2022-05-27 21:11:38 -0400

    updated probability checking

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-var clearButton;
-var doodleClassifier;
-var resultsDiv;
-var img;
-var sunModel = document.querySelector("#sunModel");
-var carModel = document.querySelector("#carModel");
-var cloudsModel = document.querySelector("#cloudsModel");
-var natureModel = document.querySelector("#natureModel");
-var houseModel = document.querySelector("#houseModel");
-var recognizedText = document.querySelector("#recognizedText");
+var myButton = document.querySelector("#recognize"),
+  myButton2 = document.querySelector("#erase"),
+  plane = document.querySelector("#drawingArea"),
+  clearButton,
+  sunModel = document.querySelector("#sunModel"),
+  carModel = document.querySelector("#carModel"),
+  cloudsModel = document.querySelector("#cloudsModel"),
+  natureModel = document.querySelector("#natureModel"),
+  houseModel = document.querySelector("#houseModel"),
+  recognizedText = document.querySelector("#recognizedText");
 
 
 // Check type of device and add/remove class
@@ -17,85 +17,80 @@ if (AFRAME.utils.device.isMobile() === true || !AFRAME.utils.checkHeadsetConnect
   document.querySelector("#drawingArea").setAttribute("class", "clickable");
 }
 
-function setup() {
-  /*
-  If there is a need to use DoodleNet, use the code below: 
-  doodleClassifier = ml5.imageClassifier('DoodleNet', modelReady);
-  */
-  // Own trained classifier
-  doodleClassifier = ml5.imageClassifier('model/model.json', modelReady);
-  var myButton = document.querySelector("#recognize"),
-    myButton2 = document.querySelector("#erase"),
-    plane = document.querySelector("#drawingArea");
 
-  // Get texure
-  myButton.addEventListener("click", function () {
-    var mesh3D = plane.getObject3D('mesh');
-    // Get canvas texture and convert to dataURL
-    texture = mesh3D.material.map.image.toDataURL();
-    doClassification(texture);
-  })
-  // Erase drawn
-  myButton2.addEventListener("click", function () {
-    plane.setAttribute("texture-painter", "clearAll: true");
-    plane.setAttribute("texture-painter", "clearAll: false");
-  })
-}
 
-// Do classification
-function doClassification(texture) {
-  img = createImg(texture, imageReady);
-  doodleClassifier.classify(img, gotResults);
-}
+// Get texure
+myButton.addEventListener("click", function () {
+  var mesh3D = plane.getObject3D('mesh');
+  // Get canvas texture and convert to dataURL
+  var texture = mesh3D.material.map.image.toDataURL();
+  // Add material to img container
+  document.getElementById("drawn").setAttribute("src", texture);
+  // Do the prediction
+  init().then(() => {
+    predict();
+  });
+})
+// Erase drawn
+myButton2.addEventListener("click", function () {
+  plane.setAttribute("texture-painter", "clearAll: true");
+  plane.setAttribute("texture-painter", "clearAll: false");
+})
 
-// Check for readiness
-function modelReady() {
-  console.log('model loaded');
 
-}
-
-// Show results
-function gotResults(error, results) {
-  if (error) {
-    console.error(error);
-    return;
+// Load model
+const URL = 'model/';
+let model, labelContainer, maxPredictions;
+// Load the image model
+async function init() {
+  const modelURL = URL + 'model.json';
+  const metadataURL = URL + 'metadata.json';
+  // load the model and metadata
+  model = await tmImage.load(modelURL, metadataURL);
+  maxPredictions = model.getTotalClasses();
+  labelContainer = document.getElementById('label-container');
+  for (let i = 0; i < maxPredictions; i++) {
+    // and class labels
+    labelContainer.appendChild(document.createElement('div'));
   }
+}
 
-  // Change float to number
-  var confidenceLevel = results[0].confidence.toFixed(2) * 100;
-  // If confidence level is >= 90, show the result
-  if (confidenceLevel >= 90) {
-    var content = "I see: " + results[0].label + '. My confidence is: ' + confidenceLevel + "%. Creating virtual  " + results[0].label + "...";
-    recognizedText.setAttribute("text", "value:" + content);
-    if (results[0].label === "house") {
-      setTimeout(() => {
-        houseModel.setAttribute("visible", true);
-        recognizedText.setAttribute("text", "value: Console");
-      }, 3000);
-    } else if (results[0].label === "sun") {
-      setTimeout(() => {
-        sunModel.setAttribute("visible", true);
-        recognizedText.setAttribute("text", "value: Console");
-      }, 3000);
-    } else if (results[0].label === "car") {
-      setTimeout(() => {
-        carModel.setAttribute("visible", true);
-        recognizedText.setAttribute("text", "value: Console");
-      }, 3000);
-    } else if (results[0].label === "clouds") {
-      setTimeout(() => {
-        cloudsModel.setAttribute("visible", true);
-        recognizedText.setAttribute("text", "value: Console");
-      }, 3000);
-    } else if (results[0].label === "nature") {
-      setTimeout(() => {
-        natureModel.setAttribute("visible", true);
-        recognizedText.setAttribute("text", "value: Console");
-      }, 3000);
+// Predict
+async function predict() {
+  var image = document.getElementById("drawn");
+  const prediction = await model.predict(image, false);
+  for (let i = 0; i < 5; i++) {
+    if(prediction[i].probability.toFixed(2) * 100 >= 90){
+      recognizedText.setAttribute("text", "value: " + "I see: " +  prediction[i].className + ". Probability level is: " + prediction[i].probability.toFixed(2) * 100 + "%. Creating virtual " + prediction[i].className);
     }
+    
+   
   }
-}
-
-function imageReady() {
-  image(img, 0, 0, width, height);
+  if (prediction[0].probability.toFixed(2) * 100 >= 90) {
+    setTimeout(() => {
+      natureModel.setAttribute("visible", true);
+      recognizedText.setAttribute("text", "value: Console");
+    }, 3000);
+   
+  } else if (prediction[1].probability.toFixed(2) * 100 >= 90) {
+    setTimeout(() => {
+      houseModel.setAttribute("visible", true);
+      recognizedText.setAttribute("text", "value: Console");
+    }, 3000);
+  } else if (prediction[2].probability.toFixed(2) * 100 >= 90) {
+    setTimeout(() => {
+      cloudsModel.setAttribute("visible", true);
+      recognizedText.setAttribute("text", "value: Console");
+    }, 3000);
+  } else if (prediction[3].probability.toFixed(2) * 100 >= 90) {
+    setTimeout(() => {
+      sunModel.setAttribute("visible", true);
+      recognizedText.setAttribute("text", "value: Console");
+    }, 3000);
+  } else if (prediction[4].probability.toFixed(2) * 100 >= 90) {
+    setTimeout(() => {
+      carModel.setAttribute("visible", true);
+      recognizedText.setAttribute("text", "value: Console");
+    }, 3000);
+  }
 }
